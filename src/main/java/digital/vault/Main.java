67a010da77fbe945ea57bdbd3813a2de0c6f040b
Fernando-Emanuel Donea <fernandodonea10@gmail.com>
2026-06-11 11:55:24 +0300

    update: switch from local memory to database

--- src/main/java/digital/vault/Main.java
+++ src/main/java/digital/vault/Main.java
@@ -3,7 +3,9 @@ package digital.vault;
 
 import digital.vault.dao.UserDao;
 import digital.vault.dao.VaultDao;
+import digital.vault.dao.impl.UserDaoDB;
 import digital.vault.dao.impl.UserDaoInMemory;
+import digital.vault.dao.impl.VaultDaoDB;
 import digital.vault.dao.impl.VaultDaoInMemory;
 import digital.vault.service.AuthService;
 import digital.vault.service.VaultService;
@@ -11,10 +13,12 @@ import digital.vault.ui.TerminalService;
 
 
 public class Main {
-    static void main()
+    public static void main()
     {
-        UserDao userDao=new UserDaoInMemory();
-        VaultDao vaultDao=new VaultDaoInMemory();
+        UserDao userDao=new UserDaoDB();
+        VaultDao vaultDao=new VaultDaoDB();
+
+
 
 
         AuthService authService=new AuthService(userDao);
