da897ac1e4ec45f1fc704c9cf540b340e4189e3f
Fernando-Emanuel Donea <fernandodonea10@gmail.com>
2026-06-23 18:22:26 +0300

    fix: Database connection before running

--- src/main/java/digital/vault/Main.java
+++ src/main/java/digital/vault/Main.java
@@ -7,6 +7,8 @@ import digital.vault.dao.impl.UserDaoDB;
 import digital.vault.dao.impl.UserDaoInMemory;
 import digital.vault.dao.impl.VaultDaoDB;
 import digital.vault.dao.impl.VaultDaoInMemory;
+import digital.vault.db.Database;
+import digital.vault.exception.DatabaseException;
 import digital.vault.service.AuthService;
 import digital.vault.service.VaultService;
 import digital.vault.ui.TerminalService;
@@ -15,19 +17,23 @@ import digital.vault.ui.TerminalService;
 public class Main {
     public static void main()
     {
-        UserDao userDao=new UserDaoDB();
-        VaultDao vaultDao=new VaultDaoDB();
+        try{
+            
+            Database.getInstance().getConnection();
 
+            UserDao userDao=new UserDaoDB();
+            VaultDao vaultDao=new VaultDaoDB();
 
+            AuthService authService=new AuthService(userDao);
+            VaultService vaultService=new VaultService(vaultDao, authService);
 
-
-        AuthService authService=new AuthService(userDao);
-        VaultService vaultService=new VaultService(vaultDao, authService);
-
-
-        TerminalService terminal = new TerminalService(vaultService, authService);
-        terminal.start();
-
-
+            TerminalService terminal = new TerminalService(vaultService, authService);
+            terminal.start();
+        }
+        catch(DatabaseException e)
+        {
+            System.out.println("Databse not available: "+e.getMessage());
+            System.out.println("Try running `docker compose up -d` to start the database");
+        }
     }
 }
